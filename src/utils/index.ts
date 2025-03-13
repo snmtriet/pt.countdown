7094da4375657501f1e579d697ee00ac89bba029
Su Nguyen Minh Triet <75319942+snmtriet@users.noreply.github.com>
2025-03-13 20:14:13 +0700

    fix: sound play wrong

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,146 +1,154 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { clsx, type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
-import { Howl } from 'howler'
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { Howl } from "howler";
 
 const soundMap10 = new Howl({
-  src: ['/sounds/map-10.mp3'],
+  src: ["/sounds/map-10.mp3"],
   preload: true,
-})
+});
 const soundMap9 = new Howl({
-  src: ['/sounds/map-9.mp3'],
+  src: ["/sounds/map-9.mp3"],
   preload: true,
-})
+});
 const soundMap8 = new Howl({
-  src: ['/sounds/map-8.mp3'],
+  src: ["/sounds/map-8.mp3"],
   preload: true,
-})
+});
 const soundMap7 = new Howl({
-  src: ['/sounds/map-7.mp3'],
+  src: ["/sounds/map-7.mp3"],
   preload: true,
-})
+});
 const soundMap6 = new Howl({
-  src: ['/sounds/map-6.mp3'],
+  src: ["/sounds/map-6.mp3"],
   preload: true,
-})
+});
 const soundMap5 = new Howl({
-  src: ['/sounds/map-5.mp3'],
+  src: ["/sounds/map-5.mp3"],
   preload: true,
-})
+});
 const soundMap4 = new Howl({
-  src: ['/sounds/map-4.mp3'],
+  src: ["/sounds/map-4.mp3"],
   preload: true,
-})
+});
 const soundMap3 = new Howl({
-  src: ['/sounds/map-3.mp3'],
+  src: ["/sounds/map-3.mp3"],
   preload: true,
-})
+});
 const soundMap2 = new Howl({
-  src: ['/sounds/map-2.mp3'],
+  src: ["/sounds/map-2.mp3"],
   preload: true,
-})
+});
 const soundMap1 = new Howl({
-  src: ['/sounds/map-1.mp3'],
+  src: ["/sounds/map-1.mp3"],
   preload: true,
-})
+});
 
 const sound10 = new Howl({
-  src: ['/sounds/10.mp3'],
+  src: ["/sounds/10.mp3"],
   preload: true,
-})
+});
 const sound9 = new Howl({
-  src: ['/sounds/9.mp3'],
+  src: ["/sounds/9.mp3"],
   preload: true,
-})
+});
 const sound8 = new Howl({
-  src: ['/sounds/8.mp3'],
+  src: ["/sounds/8.mp3"],
   preload: true,
-})
+});
 const sound7 = new Howl({
-  src: ['/sounds/7.mp3'],
+  src: ["/sounds/7.mp3"],
   preload: true,
-})
+});
 const sound6 = new Howl({
-  src: ['/sounds/6.mp3'],
+  src: ["/sounds/6.mp3"],
   preload: true,
-})
+});
 const sound5 = new Howl({
-  src: ['/sounds/5.mp3'],
+  src: ["/sounds/5.mp3"],
   preload: true,
-})
+});
 const sound4 = new Howl({
-  src: ['/sounds/4.mp3'],
+  src: ["/sounds/4.mp3"],
   preload: true,
-})
+});
 const sound3 = new Howl({
-  src: ['/sounds/3.mp3'],
+  src: ["/sounds/3.mp3"],
   preload: true,
-})
+});
 const sound2 = new Howl({
-  src: ['/sounds/2.mp3'],
+  src: ["/sounds/2.mp3"],
   preload: true,
-})
+});
 const sound1 = new Howl({
-  src: ['/sounds/1.mp3'],
+  src: ["/sounds/1.mp3"],
   preload: true,
-})
+});
 const soundRemainOneMinute = new Howl({
-  src: ['/sounds/remain-one-minute.mp3'],
+  src: ["/sounds/remain-one-minute.mp3"],
   preload: true,
-})
+});
 const soundRemainTwoMinute = new Howl({
-  src: ['/sounds/remain-two-minute.mp3'],
+  src: ["/sounds/remain-two-minute.mp3"],
   preload: true,
-})
+});
 const soundRemainThirtySecond = new Howl({
-  src: ['/sounds/remain-thirty-seconds.mp3'],
+  src: ["/sounds/remain-thirty-seconds.mp3"],
   preload: true,
-})
+});
 const soundCardAppear = new Howl({
-  src: ['/sounds/card-appear.mp3'],
+  src: ["/sounds/card-appear.mp3"],
   preload: true,
-})
+});
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-export const playSounds = async (minute: number, sound: Howl) => {
+export const playSounds = (minute: number, sound: Howl) => {
+  let firstSound: Howl | null = null;
+
   switch (minute) {
     case 10:
-      soundMap10.play()
-      break
+      firstSound = soundMap10;
+      break;
     case 9:
-      soundMap9.play()
-      break
+      firstSound = soundMap9;
+      break;
     case 8:
-      soundMap8.play()
-      break
+      firstSound = soundMap8;
+      break;
     case 7:
-      soundMap7.play()
-      break
+      firstSound = soundMap7;
+      break;
     case 6:
-      soundMap6.play()
-      break
+      firstSound = soundMap6;
+      break;
     case 5:
-      soundMap5.play()
-      break
+      firstSound = soundMap5;
+      break;
     case 4:
-      soundMap4.play()
-      break
+      firstSound = soundMap4;
+      break;
     case 3:
-      soundMap3.play()
-      break
+      firstSound = soundMap3;
+      break;
     case 2:
-      soundMap2.play()
-      break
+      firstSound = soundMap2;
+      break;
     case 1:
-      soundMap1.play()
-      break
+      firstSound = soundMap1;
+      break;
   }
-  await new Promise((resolve) => setTimeout(resolve, 1200))
-  sound.play()
-}
+
+  if (!firstSound) return;
+
+  firstSound.once("end", () => {
+    sound.play();
+  });
+
+  firstSound.play();
+};
 
 export {
   soundMap10,
@@ -167,4 +175,4 @@ export {
   soundRemainTwoMinute,
   soundRemainThirtySecond,
   soundCardAppear,
-}
+};
